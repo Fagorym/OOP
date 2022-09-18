738f140552f29c0c7fb97d94523229fcc6392556
Fagor <61354020+Fagorym@users.noreply.github.com>
2022-09-18 12:34:11 +0700

    added creating heap from args

--- task_1_1_1/src/main/java/nsu/fit/veber/Main.java
+++ task_1_1_1/src/main/java/nsu/fit/veber/Main.java
@@ -2,8 +2,11 @@ package nsu.fit.veber;
 
 public class Main {
     public static void main(String[] args) {
-        int [] myArray = new int[]{-1,2,3,51,23,45,124,2334,575,22,34,1,-100,-2303532};
-        Heap heap = new Heap(myArray);
+        int[] result = new int[args.length];
+        for (int i = 0; i < args.length; i++) {
+            result[i] = Integer.parseInt(args[i]);
+        }
+        Heap heap = new Heap(result);
         heap.sort();
         heap.print();
     }
