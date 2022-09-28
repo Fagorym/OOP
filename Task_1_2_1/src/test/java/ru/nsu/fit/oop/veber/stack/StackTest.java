dedc7bb0783677dc7ca1ff723cf776e8980f8334
Fagor <oleg-veber@mail.ru>
2022-09-28 17:54:56 +0700

    Other test created

--- Task_1_2_1/src/test/java/ru/nsu/fit/oop/veber/stack/StackTest.java
+++ Task_1_2_1/src/test/java/ru/nsu/fit/oop/veber/stack/StackTest.java
@@ -5,10 +5,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.EmptyStackException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class stackTest {
+public class StackTest {
     int count = 100;
     private Stack stack;
 
@@ -21,8 +20,7 @@ public class stackTest {
     private void popEveryElement(Stack stack, int count) {
         for (int i = count - 1; i >= 0; i--) {
             var curElem = stack.pop();
-            var expectedElem = i;
-            assertEquals(curElem, expectedElem);
+            assertEquals(curElem, i);
         }
     }
 
@@ -48,6 +46,35 @@ public class stackTest {
         pushElements(temp, count);
         stack.pushStack(temp);
         popEveryElement(stack, count);
+    }
+
+    @Test
+    void testPushFullStack() {
+        Stack littleStack = new Stack(0);
+        assertFalse(littleStack.push(1));
+    }
+
+    @Test
+    void testPushStackFullStack() {
+        Stack littleStack = new Stack(0);
+        stack.push(1);
+        assertFalse(littleStack.pushStack(stack));
+    }
+
+    @Test
+    void testPopStackEmptyStack() {
+        assertThrows(EmptyStackException.class, () -> stack.popStack(4));
+    }
+
+    @Test
+    void testCheckPopStack() {
+        Stack temp = new Stack();
+        pushElements(temp, count);
+        stack.pushStack(temp);
+        Stack checkStack = stack.popStack(count);
+        for (int i = count - 1; i >= 0; i--) {
+            assertEquals(i, checkStack.pop());
+        }
     }
 
 
