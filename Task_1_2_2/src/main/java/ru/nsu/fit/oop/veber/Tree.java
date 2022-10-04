7fe925493cded887666acc11b9b4aa1b75e6dfe0
Fagor <oleg-veber@mail.ru>
2022-10-04 20:29:54 +0700

    implement add

--- Task_1_2_2/src/main/java/ru/nsu/fit/oop/veber/Tree.java
+++ Task_1_2_2/src/main/java/ru/nsu/fit/oop/veber/Tree.java
@@ -1,26 +1,26 @@
 package ru.nsu.fit.oop.veber;
-    
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.stream.Stream;
 
-public class Tree<T> implements Collection {
+public class Tree<T> implements Collection<T> {
     private Node<T> root;
-    private int size = 0;
 
     @Override
     public int size() {
-        return this.size;
+        return this.root.size;
     }
 
     @Override
     public boolean isEmpty() {
-        return this.size == 0;
+        return this.root.size == 0;
     }
 
     @Override
     public boolean contains(Object o) {
+        checkNotNull(o);
         return false;
     }
 
@@ -36,17 +36,34 @@ public class Tree<T> implements Collection {
 
     @Override
     public boolean add(Object o){
-        return false;
+        add(root, o);
+        return true;
+    }
+
+    @SuppressWarnings("unchecked")
+    public boolean add(Node<T> node, Object o){
+        checkNotNull(o);
+        Node<T> newNode = new Node<>();
+        newNode.elem = (T)o;
+        node.Nodes.add(newNode);
+        node.size++;
+        return true;
     }
 
     @Override
     public boolean remove(Object o) {
+        checkNotNull(o);
         return false;
     }
 
     @Override
     public boolean addAll(Collection c) {
-        return false;
+        c.forEach(this::add);
+        return true;
+    }
+    public boolean addAll(Node<T> node, Collection c) {
+        c.forEach(o -> add(node,o));
+        return true;
     }
 
     @Override
@@ -76,6 +93,13 @@ public class Tree<T> implements Collection {
 
     private class Node<T> {
         T elem;
-        ArrayList<T> Nodes;
+        private int size = 0;
+        ArrayList<Node <T>> Nodes;
+    }
+
+    private void checkNotNull(Object o){
+        if (o == null){
+            throw new NullPointerException();
+        }
     }
 }
