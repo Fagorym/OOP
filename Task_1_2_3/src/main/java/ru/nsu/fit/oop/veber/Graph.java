969259eb4c9d30733e42a5c3c614775daed4d294
Fagor <oleg-veber@mail.ru>
2022-10-23 21:59:58 +0700

    Realize some javadocs + fix errors

--- Task_1_2_3/src/main/java/ru/nsu/fit/oop/veber/Graph.java
+++ Task_1_2_3/src/main/java/ru/nsu/fit/oop/veber/Graph.java
@@ -5,14 +5,38 @@ import java.util.Set;
 
 public interface Graph<T> {
     public boolean addVertex(Vertex<T> vertex);
+
     public void deleteVertex(Vertex<T> vertex);
+
     public void addEdge(Edge<T> edge);
+
     public void deleteEdge(Edge<T> edge);
+
     public Set<Vertex<T>> getAdjVertexes(Vertex<T> vertex);
+
     public T getVertexElement(Vertex<T> vertex);
+
     public void setVertexElement(Vertex<T> vertex, T newElem);
 
+    /**
+     * Method returns degree of current vertex.
+     *
+     * @param vertex - from what vertex we count degree
+     * @return degree of the vertex (count of edges those are connected to it)
+     */
     public int getVertexDegree(Vertex<T> vertex);
-    public int getVertexNumber(Vertex<T> vertex);
-    public int getEdgesNumber(Vertex<T> vertex);
+
+    /**
+     * Method returns number of vertexes in graph.
+     *
+     * @return number of vertexes
+     */
+    public int getVertexNumber();
+
+    /**
+     * Method returns number of edges from current vertex.
+     *
+     * @return count of edges
+     */
+    public int getEdgesNumber();
 }
--- Task_1_2_3/src/main/java/ru/nsu/fit/oop/veber/Vertex.java
+++ Task_1_2_3/src/main/java/ru/nsu/fit/oop/veber/Vertex.java
@@ -1,25 +1,25 @@
 package ru.nsu.fit.oop.veber;
 
-import java.util.ArrayList;
+
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class Vertex<T> {
     T elem;
     Set<Edge<T>> startEdges;
     Set<Edge<T>> endEdges;
-    public Vertex (T elem){
+
+    public Vertex(T elem) {
         this.elem = elem;
         this.startEdges = new HashSet<>();
         this.endEdges = new HashSet<>();
     }
 
-    public void addStartEdge(Edge<T> edge){
+    public void addStartEdge(Edge<T> edge) {
         this.startEdges.add(edge);
     }
 
-    public void addEndEdge(Edge<T> edge){
+    public void addEndEdge(Edge<T> edge) {
         this.endEdges.add(edge);
     }
 
