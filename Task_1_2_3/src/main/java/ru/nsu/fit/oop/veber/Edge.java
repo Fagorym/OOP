c4d81c59c0558b578616db3e860a0051e6668d1c
Fagor <oleg-veber@mail.ru>
2022-10-24 11:56:05 +0700

    start implement toString() method

--- Task_1_2_3/src/main/java/ru/nsu/fit/oop/veber/Edge.java
+++ Task_1_2_3/src/main/java/ru/nsu/fit/oop/veber/Edge.java
@@ -2,12 +2,17 @@ package ru.nsu.fit.oop.veber;
 
 public class Edge<T> {
     int weight;
+
+    String name;
     Vertex<T> start;
     Vertex<T> end;
 
-    public Edge(int weight, Vertex<T> start, Vertex<T> end){
+    public Edge(String name, int weight, Vertex<T> start, Vertex<T> end){
         this.weight = weight;
         this.start = start;
+        start.startEdges.add(this);
         this.end = end;
+        end.endEdges.add(this);
+        this.name = name;
     }
 }
--- Task_1_2_3/src/test/java/ru.nsu.fit.oop.veber/IncMatrixTest.java
+++ Task_1_2_3/src/test/java/ru.nsu.fit.oop.veber/IncMatrixTest.java
@@ -1,4 +1,28 @@
 package ru.nsu.fit.oop.veber;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class IncMatrixTest {
+    IncMatrix<String> testedMatrix;
+
+    @BeforeEach
+    public void initMatrix(){
+        testedMatrix = new IncMatrix<>();
+    }
+
+    @Test
+    public void testToStringMethod(){
+        Vertex<String> firstVertex = new Vertex<>("A");
+        Vertex<String> secondVertex = new Vertex<>("B");
+        Vertex<String> thirdVertex = new Vertex<>("C");
+        Edge<String> firstEdge = new Edge<>("1", 12, firstVertex, secondVertex);
+        Edge<String> secondEdge = new Edge<>("2", 43, firstVertex, thirdVertex);
+        testedMatrix.addVertex(firstVertex);
+        testedMatrix.addVertex(secondVertex);
+        testedMatrix.addVertex(thirdVertex);
+        testedMatrix.addEdge(firstEdge);
+        testedMatrix.addEdge(secondEdge);
+        System.out.println(testedMatrix.toString());
+    }
 }
