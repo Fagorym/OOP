778f3fd7b357889a560dc1a4f568cb4995ad3714
Fagor <oleg-veber@mail.ru>
2022-11-30 11:23:26 +0700

    add javadocs for semester

--- Task_1_4_1/src/main/java/ru/nsu/fit/oop/veber/Semester/Semester.java
+++ Task_1_4_1/src/main/java/ru/nsu/fit/oop/veber/Semester/Semester.java
@@ -2,6 +2,9 @@ package ru.nsu.fit.oop.veber.Semester;
 
 import java.util.HashMap;
 
+/**
+ * Class that implements current semester and its grades.
+ */
 public class Semester {
     private final HashMap<String, Integer> grades;
 
@@ -10,6 +13,11 @@ public class Semester {
     private int subjectCount;
 
 
+    /**
+     * Default constructor of semester.
+     *
+     * @param semesterNumber - number of current semester
+     */
     public Semester(int semesterNumber) {
         this.grades = new HashMap<>();
         averageGrade = 0;
@@ -18,6 +26,12 @@ public class Semester {
     }
 
 
+    /**
+     * Method that add new pair in hashmap, where subject is key, grade is value.
+     *
+     * @param subject - will be key
+     * @param grade   - will be value
+     */
     public void addGrade(String subject, Integer grade) {
         if (grade < 1 || grade > 5) {
             throw new IllegalArgumentException("Grade cannot be less than 1 and greater than 5");
@@ -29,6 +43,9 @@ public class Semester {
         recountAverage();
     }
 
+    /**
+     * Method that refresh average grade. It used every time, after we add new mark to the semester.
+     */
     private void recountAverage() {
         averageGrade = 0;
         for (Integer grade : grades.values()) {
@@ -38,25 +55,45 @@ public class Semester {
     }
 
 
+    /**
+     * Getter for average grade.
+     *
+     * @return average grade in this semester
+     */
     public float getAverageGrade() {
         return averageGrade;
     }
 
+    /**
+     * Getter for count of subject.
+     *
+     * @return count of subjects in this semester
+     */
     public int getSubjectCount() {
         return subjectCount;
     }
 
-    public Object[] getGradesArray(){
+    /**
+     * Method that return grades as array.
+     *
+     * @return array of grades in this semester
+     */
+    public Object[] getGradesArray() {
         return this.grades.values().toArray();
     }
 
 
+    /**
+     * Method that convert this semester to string.
+     *
+     * @return string representation of this semester
+     */
     @Override
-    public String toString(){
+    public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("Semester number ").append(semesterNumber).append('\n');
         stringBuilder.append("================================").append('\n');
-        for (String subject: grades.keySet()){
+        for (String subject : grades.keySet()) {
             stringBuilder.append(subject).append(" - ").append(grades.get(subject)).append('\n');
         }
         stringBuilder.append("Average grade: ").append(getAverageGrade()).append("\n");
