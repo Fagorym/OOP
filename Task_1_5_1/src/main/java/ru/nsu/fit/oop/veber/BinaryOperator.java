d844baa4f32b2b2d1967241188c9ad98dfd79e86
Fagor <oleg-veber@mail.ru>
2022-12-07 11:23:12 +0700

    implements plus, minus, miltiple and divide

--- Task_1_5_1/src/main/java/ru/nsu/fit/oop/veber/BinaryOperator.java
+++ Task_1_5_1/src/main/java/ru/nsu/fit/oop/veber/BinaryOperator.java
@@ -4,34 +4,37 @@ public interface BinaryOperator extends Operator {
 
     int argCount = 2;
 
+
+    Float calculate(Float fst, Float snd);
+
     public class Plus implements BinaryOperator {
 
         @Override
-        public Float calculate() {
-            return null;
+        public Float calculate(Float fst, Float snd) {
+            return fst + snd;
         }
     }
 
     public class Minus implements BinaryOperator {
 
         @Override
-        public Float calculate() {
-            return null;
+        public Float calculate(Float fst, Float snd) {
+            return fst - snd;
         }
     }
 
     public class Multiple implements BinaryOperator {
 
         @Override
-        public Float calculate() {
-            return null;
+        public Float calculate(Float fst, Float snd) {
+            return fst * snd;
         }
     }
 
     public class Divide implements BinaryOperator {
         @Override
-        public Float calculate() {
-            return null;
+        public Float calculate(Float fst, Float snd) {
+            return fst / snd;
         }
     }
 }
