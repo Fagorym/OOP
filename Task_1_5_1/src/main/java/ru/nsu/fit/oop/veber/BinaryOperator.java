1b9b9928784bf4ba06b81c57fcbe939156b78c6e
Fagor <oleg-veber@mail.ru>
2022-12-07 10:30:48 +0700

    Binary operator implements

--- Task_1_5_1/src/main/java/ru/nsu/fit/oop/veber/BinaryOperator.java
+++ Task_1_5_1/src/main/java/ru/nsu/fit/oop/veber/BinaryOperator.java
@@ -1,4 +1,37 @@
 package ru.nsu.fit.oop.veber;
 
 public interface BinaryOperator extends Operator {
+
+    int argCount = 2;
+
+    public class Plus implements BinaryOperator {
+
+        @Override
+        public Float calculate() {
+            return null;
+        }
+    }
+
+    public class Minus implements BinaryOperator {
+
+        @Override
+        public Float calculate() {
+            return null;
+        }
+    }
+
+    public class Multiple implements BinaryOperator {
+
+        @Override
+        public Float calculate() {
+            return null;
+        }
+    }
+
+    public class Divide implements BinaryOperator {
+        @Override
+        public Float calculate() {
+            return null;
+        }
+    }
 }
