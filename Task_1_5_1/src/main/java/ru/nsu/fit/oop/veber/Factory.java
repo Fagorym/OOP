2414a8b380c1d866025cdb347916d57e877888e7
Fagor <oleg-veber@mail.ru>
2022-12-19 20:58:16 +0700

    wrote javadocs

--- Task_1_5_1/src/main/java/ru/nsu/fit/oop/veber/Factory.java
+++ Task_1_5_1/src/main/java/ru/nsu/fit/oop/veber/Factory.java
@@ -3,23 +3,47 @@ package ru.nsu.fit.oop.veber;
 import java.util.HashMap;
 import java.util.Set;
 
+/**
+ * Class that represent operator factory.
+ */
 public class Factory {
 
     private final HashMap<String, Operator> factory;
 
+    /**
+     * Main constructor of the factory.
+     * Created hashmap and init basic operators.
+     */
     public Factory() {
         factory = new HashMap<>();
         initOperatorMap();
     }
 
+    /**
+     * Adds custom operator to factory.
+     *
+     * @param key      - will be string representation of the operator
+     * @param operator - operator interface, that implemented main calculate operation for this operator
+     */
     public void addOperator(String key, Operator operator) {
         factory.put(key, operator);
     }
 
+    /**
+     * Getter for the operator.
+     *
+     * @param key - key of the explored operator
+     * @return operator by key
+     */
     public Operator getOperator(String key) {
         return factory.get(key);
     }
 
+    /**
+     * Getter for set of all operators, those fabric contains.
+     *
+     * @return set of all operators
+     */
     public Set<String> getOperators() {
         return factory.keySet();
     }
