9e8b428e5303936419669ff44637821832fb659a
Fagor <oleg-veber@mail.ru>
2022-12-19 20:58:16 +0700

    wrote javadocs

--- Task_1_5_1/src/main/java/ru/nsu/fit/oop/veber/Calculator.java
+++ Task_1_5_1/src/main/java/ru/nsu/fit/oop/veber/Calculator.java
@@ -3,26 +3,47 @@ package ru.nsu.fit.oop.veber;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
+/**
+ * Class that represent calculator.
+ */
 public class Calculator {
     private final Deque<Float> floatDeque;
     private final Factory factory;
     private String expression;
 
+    /**
+     * Main constructor of calculator, with empty expression.
+     */
     public Calculator() {
         this.floatDeque = new ArrayDeque<>();
         this.factory = new Factory();
     }
 
+    /**
+     * Additional constructor of calculator, with non-empty expression.
+     *
+     * @param expression - will be calculated expression
+     */
     public Calculator(String expression) {
         this();
         this.expression = expression;
 
     }
 
+    /**
+     * Setter for expression.
+     *
+     * @param expression - will be calculated expression
+     */
     public void setExpression(String expression) {
         this.expression = expression;
     }
 
+    /**
+     * Counts the expression, that was settled by constructor or setter.
+     *
+     * @return result of the calculated expression
+     */
     public Float calculate() {
         String[] tokens = expression.split(" ");
         for (int i = tokens.length - 1; i >= 0; i--) {
--- Task_1_5_1/src/main/java/ru/nsu/fit/oop/veber/Operator.java
+++ Task_1_5_1/src/main/java/ru/nsu/fit/oop/veber/Operator.java
@@ -1,4 +1,7 @@
 package ru.nsu.fit.oop.veber;
 
+/**
+ * Marker interface for operators.
+ */
 public interface Operator {
 }
