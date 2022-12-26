cc1ba2184f81475de09f73ddcba58c84c33b1b03
Fagorym <Fagorym@users.noreply.github.com>
2022-12-26 20:24:51 +0000

    Deploying to gh-pages from @ Fagorym/OOP@cf7e510ab06d8f04ad153e08615ba95f3b9a3334 🚀

--- Task_1_5_2/package-search-index.js
+++ Task_1_5_2/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.fit.oop.veber"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.fit.oop.veber"},{"l":"ru.nsu.fit.oop.veber.recordbook"},{"l":"ru.nsu.fit.oop.veber.utils"}];updateSearchResults();
