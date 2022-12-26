8ba52a88941f2b5233da31f257ae0b4d31c94b1b
Fagor <oleg-veber@mail.ru>
2022-12-26 14:34:00 +0700

    Make tests for recordbook

--- Task_1_5_2/src/main/java/ru/nsu/fit/oop/veber/Main.java
+++ Task_1_5_2/src/main/java/ru/nsu/fit/oop/veber/Main.java
@@ -5,8 +5,27 @@ import picocli.CommandLine;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        new CommandLine(new RecordBook()).execute(args);
 
+    private static RecordBook book = new RecordBook();
+
+    public static void main(String[] args) throws IOException {
+        new CommandLine(book).execute(args);
+
+    }
+
+    /**
+     * Function that implemented for tests.
+     * @return actual record book
+     */
+    public RecordBook getBook() {
+        return Main.book;
+    }
+
+    /**
+     * Function that implemented for tests.
+     * @param book will be next record book.
+     */
+    public void setBook(RecordBook book) {
+        Main.book = book;
     }
 }
