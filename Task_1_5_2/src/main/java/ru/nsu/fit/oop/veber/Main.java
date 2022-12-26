fe578f9b7069275c87f623405c359870b11029e7
Fagor <oleg-veber@mail.ru>
2022-12-26 13:22:41 +0700

    made appropriate changes

--- Task_1_5_2/src/main/java/ru/nsu/fit/oop/veber/Main.java
+++ Task_1_5_2/src/main/java/ru/nsu/fit/oop/veber/Main.java
@@ -5,44 +5,8 @@ import picocli.CommandLine;
 import java.io.IOException;
 
 public class Main {
-   /* public static void main(String[] args) throws IOException {
-        if (args.length < 1) {
-            throw new IllegalArgumentException("Wrong arguments count, try again");
-        }
-        RecordBook book = new RecordBook("../records.txt");
-        switch (args[0]) {
-            case "-add" -> {
-                if (args.length < 3) {
-                    throw new IllegalArgumentException("Wrong arguments count, try again");
-                }
-                book.addRecord(args[1], args[2]);
-            }
-
-            case "-rm" -> {
-                if (args.length < 2) {
-                    throw new IllegalArgumentException("Wrong arguments count, try again");
-                }
-                book.deleteRecord(args[1]);
-            }
-
-            case "-show" -> {
-                if (args.length == 1) {
-                    book.printBook();
-
-                }
-            }
-            default -> throw new IllegalArgumentException("Wrong argument, check documentation");
-
-        }
-        book.saveBook();
-    }
-
-    */
-
     public static void main(String[] args) throws IOException {
-        RecordBook myBook = new RecordBook("../records.txt");
-        new CommandLine(myBook)
-                .setExecutionStrategy(myBook::executionStrategy)
-                .execute(args);
+        new CommandLine(new RecordBook()).execute(args);
+
     }
 }
