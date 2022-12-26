5dcf232c3b5bb2747a7a7d3334f499e7328da23e
Fagor <oleg-veber@mail.ru>
2022-12-26 13:22:41 +0700

    made appropriate changes

--- Task_1_5_2/src/main/java/ru/nsu/fit/oop/veber/RecordBook.java
+++ Task_1_5_2/src/main/java/ru/nsu/fit/oop/veber/RecordBook.java
@@ -8,45 +8,65 @@ import java.io.*;
 import java.lang.reflect.Type;
 import java.time.LocalDateTime;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import static java.lang.System.load;
 import static java.lang.System.out;
 
 
 @Command(name = "notebook")
 public class RecordBook {
-    private final File file;
+
+    @Option(names = {"-f", "-file"}, description = "Notebook file", defaultValue = "records.txt")
+    private File file;
     private TreeSet<Record> records;
 
-    public RecordBook(String filename) throws FileNotFoundException {
-        file = new File(filename);
-        loadRecords();
-        if (records == null) {
-            records = new TreeSet<>();
-        }
-    }
-
     @Command(name = "-add")
-    public void addRecord(@Option(names = "-h") String header, @Option(names = "-d") String description) {
+    public void addRecord(@Option(names = "-h", description = "Header of the note") String header,
+                          @Option(names = "-d", description = "Description of the note") String description)
+            throws IOException {
+        loadRecords();
         String datetime = String.valueOf(LocalDateTime.now());
-        Record value = new Record(datetime, header, description);
+        Record value = new Record(header, description, datetime);
+        saveRecords();
         records.add(value);
 
     }
 
-    public void printBook() {
-        out.println(this);
+    @Command(name = "-rm")
+    public void deleteRecord(@Option(names = "-h", description = "Header of the note") String header)
+            throws IOException {
+        loadRecords();
+        records.removeIf((record -> Objects.equals(record.header(), header)));
+        saveRecords();
     }
 
-    @Command(name = "-rm")
-    public void deleteRecord(String header) {
-        records.removeIf((record -> Objects.equals(record.header(), header)));
+    @Command(name = "-show")
+    public void printBook(@Option(names = "-from", description = "From what date need to find records",
+            defaultValue = Option.NULL_VALUE) String from,
+                          @Option(names = "-to", description = "To what date need to find records",
+                                  defaultValue = Option.NULL_VALUE) String to,
+                          @Option(names = "-tags", description = "Tags that must be in the header",
+                                  defaultValue = "") String[] tags) {
+        loadRecords();
+
+        LocalDateTime fromDateTime = from == null ? LocalDateTime.MIN : LocalDateTime.parse(from);
+        LocalDateTime afterDateTime = to == null ? LocalDateTime.MAX : LocalDateTime.parse(to);
+
+        records.stream()
+                .filter(record ->
+                        fromDateTime.isBefore(LocalDateTime.parse(record.createdAt())) &&
+                                afterDateTime.isAfter(LocalDateTime.parse(record.createdAt())) &&
+                                Stream.of(tags).allMatch(record.header()::contains))
+                .forEach(System.out::println);
     }
 
     public Collection<Record> getRecords() {
         return records;
     }
 
-    public void saveBook() throws IOException {
+    public void saveRecords() throws IOException {
         try (FileWriter writer = new FileWriter(file)) {
             writer.write(this.toString());
         } catch (Exception e) {
@@ -55,15 +75,6 @@ public class RecordBook {
         }
 
 
-    }
-
-    private void init() {
-
-    }
-
-    public int executionStrategy(ParseResult parseResult) {
-        init(); // custom initialization to be done before executing any command or subcommand
-        return new RunLast().execute(parseResult); // default execution strategy
     }
 
     public String toString() {
@@ -75,10 +86,12 @@ public class RecordBook {
     private void loadRecords() {
         try (FileReader reader = new FileReader(file)) {
             Gson gson = new GsonBuilder().create();
-            Type setType = new TypeToken<TreeSet<Record>>() {
-            }.getType();
-            records = gson.fromJson(reader, setType);
-        } catch (IOException e) {
+            List<Record> tempList = Arrays.asList(gson.fromJson(reader, Record[].class));
+            this.records = new TreeSet<>(tempList);
+        } catch (IOException | IllegalStateException e) {
+            records = new TreeSet<>();
+        }
+        if (records == null) {
             records = new TreeSet<>();
         }
 
