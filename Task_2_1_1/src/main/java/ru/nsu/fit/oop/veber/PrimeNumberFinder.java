0c2a3106d167de5248de37d24a9fb3f6d1e52fb2
Fagor <oleg-veber@mail.ru>
2023-02-07 15:50:44 +0700

    Initial commit

---
+++ Task_2_1_1/src/main/java/ru/nsu/fit/oop/veber/PrimeNumberFinder.java
@@ -0,0 +1,15 @@
+package ru.nsu.fit.oop.veber;
+
+public class PrimeNumberFinder {
+    public Boolean consistentFinder() {
+        return false;
+    }
+
+    public Boolean parallelFinder() {
+        return true;
+    }
+
+    public Boolean parallelStreamFinder() {
+        return false;
+    }
+}
