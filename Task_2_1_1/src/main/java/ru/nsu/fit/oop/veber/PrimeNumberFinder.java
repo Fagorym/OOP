90918c32f329ac7f2ba126a17c3fdd7f0fc9f22c
Fagor <oleg-veber@mail.ru>
2023-02-07 15:58:53 +0700

    implement consistentFinder

--- Task_2_1_1/src/main/java/ru/nsu/fit/oop/veber/PrimeNumberFinder.java
+++ Task_2_1_1/src/main/java/ru/nsu/fit/oop/veber/PrimeNumberFinder.java
@@ -1,7 +1,18 @@
 package ru.nsu.fit.oop.veber;
 
 public class PrimeNumberFinder {
+    private final Integer[] arr;
+
+    public PrimeNumberFinder(Integer[] arr) {
+        this.arr = arr;
+    }
+
     public Boolean consistentFinder() {
+        for (Integer integer : arr) {
+            if (isNotPrime(integer)) {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -10,6 +21,15 @@ public class PrimeNumberFinder {
     }
 
     public Boolean parallelStreamFinder() {
+        return false;
+    }
+
+    private boolean isNotPrime(Integer x) {
+        for (int i = 2; i < x; i++) {
+            if (x % i == 0) {
+                return true;
+            }
+        }
         return false;
     }
 }
