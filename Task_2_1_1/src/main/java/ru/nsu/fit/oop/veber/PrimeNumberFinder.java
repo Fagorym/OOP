5537d5a7ae904fce9fc3616dadef2d1c3c6763f0
Fagor <oleg-veber@mail.ru>
2023-02-11 14:35:59 +0700

    rewrite with interface

--- Task_2_1_1/src/main/java/ru/nsu/fit/oop/veber/PrimeNumberFinder.java
+++ Task_2_1_1/src/main/java/ru/nsu/fit/oop/veber/PrimeNumberFinder.java
@@ -1,53 +1,15 @@
 package ru.nsu.fit.oop.veber;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.List;
+public interface PrimeNumberFinder {
 
-public class PrimeNumberFinder {
-    private final Util util = new Util();
-    private final Integer[] arr;
+    Boolean haveNotPrime();
 
-    public PrimeNumberFinder(Integer[] arr) {
-        this.arr = arr;
-    }
-
-    public Boolean consistentFinder() {
-        for (Integer integer : arr) {
-            if (util.isNotPrime(integer)) {
+    default Boolean isNotPrime(Integer x) {
+        for (int i = 2; i < x; i++) {
+            if (x % i == 0) {
                 return true;
             }
         }
         return false;
-    }
-
-    public Boolean parallelFinder() {
-        int threadCount = Thread.activeCount();
-        Boolean interruptAll = false;
-        boolean isAlive = true;
-        CustomThread[] threads = new CustomThread[threadCount];
-        Deque<Integer> deque = new ArrayDeque<>(List.of(arr));
-        for (int i = 0; i < threadCount; i++) {
-            threads[i] = new CustomThread(deque, false);
-            threads[i].start();
-        }
-        while (isAlive) {
-            isAlive = false;
-            for (int i = 0; i < threadCount; i++) {
-                if (threads[i].getValue() || interruptAll) {
-                    interruptAll = true;
-                    threads[i].interrupt();
-                } else if (threads[i].isAlive()) {
-                    isAlive = true;
-                }
-            }
-        }
-        return interruptAll;
-    }
-
-    public Boolean parallelStreamFinder() {
-        List<Integer> list = Arrays.stream(arr).parallel().filter(util::isNotPrime).toList();
-        return !list.isEmpty();
     }
 }
--- Task_2_1_1/src/test/java/ru/nsu/fit/oop/veber/test/PrimeNumberFinderTest.java
+++ Task_2_1_1/src/test/java/ru/nsu/fit/oop/veber/test/PrimeNumberFinderTest.java
@@ -2,32 +2,27 @@ package ru.nsu.fit.oop.veber.test;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import ru.nsu.fit.oop.veber.ParallelStreamPrimeNumberFinder;
 import ru.nsu.fit.oop.veber.PrimeNumberFinder;
+import ru.nsu.fit.oop.veber.PrimeNumberFinderImpl;
+import ru.nsu.fit.oop.veber.ThreadPrimeNumberFinder;
 
 public class PrimeNumberFinderTest {
-    private PrimeNumberFinder primeNumberFinder;
+    private final PrimeNumberFinder[] finders = new PrimeNumberFinder[3];
 
     @BeforeEach
     public void initFinder() {
         Integer[] arr = {4, 6997927, 6997937, 6997967, 6998009, 6998029, 6998039, 6998051, 6998053};
-        primeNumberFinder = new PrimeNumberFinder(arr);
+        finders[0] = new PrimeNumberFinderImpl(arr);
+        finders[1] = new ThreadPrimeNumberFinder(arr);
+        finders[2] = new ParallelStreamPrimeNumberFinder(arr);
     }
 
     @Test
     public void testConsistentFinder() {
-        Boolean answer = primeNumberFinder.consistentFinder();
-        System.out.println(answer);
-    }
-
-    @Test
-    public void testThreadFinder() {
-        Boolean answer = primeNumberFinder.parallelFinder();
-        System.out.println(answer);
-    }
-
-    @Test
-    public void testParallelFinder() {
-        Boolean answer = primeNumberFinder.parallelStreamFinder();
-        System.out.println(answer);
+        for (PrimeNumberFinder finder : finders) {
+            Boolean answer = finder.haveNotPrime();
+            System.out.println(answer);
+        }
     }
 }
