9bbc57a24cc97a7fcf720122964e3ad65d1abff3
Fagor <oleg-veber@mail.ru>
2023-02-13 14:59:56 +0700

    create parametrized tests

--- Task_2_1_1/src/test/java/ru/nsu/fit/oop/veber/test/PrimeNumberFinderTest.java
+++ Task_2_1_1/src/test/java/ru/nsu/fit/oop/veber/test/PrimeNumberFinderTest.java
@@ -1,28 +1,59 @@
 package ru.nsu.fit.oop.veber.test;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import ru.nsu.fit.oop.veber.ParallelStreamPrimeNumberFinder;
 import ru.nsu.fit.oop.veber.PrimeNumberFinder;
 import ru.nsu.fit.oop.veber.PrimeNumberFinderImpl;
 import ru.nsu.fit.oop.veber.ThreadPrimeNumberFinder;
 
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
 public class PrimeNumberFinderTest {
     private final PrimeNumberFinder[] finders = new PrimeNumberFinder[3];
 
-    @BeforeEach
-    public void initFinder() {
-        Integer[] arr = {4, 6997927, 6997937, 6997967, 6998009, 6998029, 6998039, 6998051, 6998053};
+    private static Stream<Arguments> arrays() {
+        return Stream.of(
+                arguments(
+                        new Integer[]{4, 6997927, 6997937, 6997967, 6998009, 6998029, 6998039, 6998051, 6998053},
+                        true
+                ),
+                arguments(
+                        new Integer[]{6997927, 6997937, 6997967, 6998009, 4, 6998029, 6998039, 6998051, 6998053},
+                        true
+                ),
+                arguments(
+                        new Integer[]{6997927, 6997937, 6997967, 6998009, 6998029, 6998039, 6998051, 6998053, 4},
+                        true
+                ),
+                arguments(
+                        new Integer[]{6997927, 6997937, 6997967, 6998009, 6998029, 6998039, 6998051, 6998053},
+                        false
+                ),
+                arguments(
+                        new Integer[]{6997927},
+                        false
+                ),
+                arguments(
+                        new Integer[]{7},
+                        false
+                ));
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("arrays")
+    public void initFinder(Integer[] arr, Boolean result) {
         finders[0] = new PrimeNumberFinderImpl(arr);
         finders[1] = new ThreadPrimeNumberFinder(arr);
         finders[2] = new ParallelStreamPrimeNumberFinder(arr);
-    }
-
-    @Test
-    public void testConsistentFinder() {
         for (PrimeNumberFinder finder : finders) {
             Boolean answer = finder.haveNotPrime();
-            System.out.println(answer);
+            Assertions.assertEquals(answer, result);
         }
     }
 }
