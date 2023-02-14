5039519d5bff2e192540e037415ffdcc836d9c2c
github-action-benchmark <github@users.noreply.github.com>
2023-02-14 04:53:57 +0000

    add JMH Benchmark (jmh) benchmark result for 1e673c34cdae1fbaae55442efcb914508475bbaa

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1676350437644,
+  "repoUrl": "https://github.com/Fagorym/OOP",
+  "entries": {
+    "JMH Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "oleg-veber@mail.ru",
+            "name": "Fagor",
+            "username": "Fagorym"
+          },
+          "committer": {
+            "email": "oleg-veber@mail.ru",
+            "name": "Fagor",
+            "username": "Fagorym"
+          },
+          "distinct": true,
+          "id": "1e673c34cdae1fbaae55442efcb914508475bbaa",
+          "message": "change file result",
+          "timestamp": "2023-02-14T11:52:15+07:00",
+          "tree_id": "303037722974e46a38d03b30f57ccda65debb723",
+          "url": "https://github.com/Fagorym/OOP/commit/1e673c34cdae1fbaae55442efcb914508475bbaa"
+        },
+        "date": 1676350436920,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "ru.nsu.fit.oop.veber.BenchmarkRunner.TestPrimeNumberFinders.sequential",
+            "value": 0.00010014078136165526,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 0\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
