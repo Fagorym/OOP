9f2f070d4138fc6656f704c0cd4a6cc60639193c
Fagor <oleg-veber@mail.ru>
2023-02-21 11:26:15 +0700

    change thread logic and tests

--- Task_2_1_1/src/main/java/ru/nsu/fit/oop/veber/ThreadPrimeNumberFinder.java
+++ Task_2_1_1/src/main/java/ru/nsu/fit/oop/veber/ThreadPrimeNumberFinder.java
@@ -1,49 +1,49 @@
 package ru.nsu.fit.oop.veber;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.List;
-import java.util.concurrent.Semaphore;
+import java.util.Arrays;
 
 public class ThreadPrimeNumberFinder implements PrimeNumberFinder {
 
     private final Integer[] arr;
-    private Integer threadCount;
+    private final Integer threadCount;
 
     public ThreadPrimeNumberFinder(Integer[] arr) {
         this.arr = arr;
         this.threadCount = Thread.activeCount();
     }
 
-    public void setThreadCount(Integer threadCount) {
-        if (threadCount <= Thread.activeCount()) {
-            this.threadCount = threadCount;
-        }
-    }
-
     @Override
     public Boolean haveNotPrime() {
+        boolean result = false;
+        checkArrayCorrectness(arr);
         CustomThread[] threads = new CustomThread[threadCount];
-        Deque<Integer> deque = new ArrayDeque<>(List.of(arr));
-        Semaphore semaphore = new Semaphore(1);
+        int tempLength = arr.length / threadCount;
+        int tempRest = arr.length % threadCount;
+        int tempPosition = 0;
         for (int i = 0; i < threadCount; i++) {
-            threads[i] = new CustomThread(deque, false, this, semaphore);
-            threads[i].start();
-        }
-
-        Boolean interruptAll = false;
-        boolean isAlive = true;
-        while (isAlive) {
-            isAlive = false;
-            for (int i = 0; i < threadCount; i++) {
-                if (threads[i].getValue() || interruptAll) {
-                    interruptAll = true;
-                    threads[i].interrupt();
-                } else if (threads[i].isAlive()) {
-                    isAlive = true;
-                }
+            Integer[] tempArray;
+            if (tempRest > 0) {
+                tempRest--;
+                tempArray = Arrays.copyOfRange(arr, tempPosition, tempPosition + tempLength + 1);
+                tempPosition += tempLength + 1;
+            } else {
+                tempArray = Arrays.copyOfRange(arr, tempPosition, tempPosition + tempLength);
+                tempPosition += tempLength;
             }
+
+            threads[i] = new CustomThread(tempArray, false, this);
+            threads[i].start();
+
         }
-        return interruptAll;
+        try {
+
+            for (int i = 0; i < threadCount; i++) {
+                threads[i].join();
+                result |= threads[i].getValue();
+
+            }
+        } catch (InterruptedException ignored) {
+        }
+        return result;
     }
 }
