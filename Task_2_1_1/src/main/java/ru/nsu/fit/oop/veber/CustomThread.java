6257c6b6c7d3f710531ed6670d1ff9f80a674379
Fagor <oleg-veber@mail.ru>
2023-03-06 20:07:45 +0700

    change thread logic (not creating new array)

--- Task_2_1_1/src/main/java/ru/nsu/fit/oop/veber/CustomThread.java
+++ Task_2_1_1/src/main/java/ru/nsu/fit/oop/veber/CustomThread.java
@@ -2,19 +2,23 @@ package ru.nsu.fit.oop.veber;
 
 public class CustomThread extends Thread {
     private final Integer[] arr;
+    private final int start;
+    private final int end;
     private final PrimeNumberFinder finder;
     private Boolean flag;
 
-    public CustomThread(Integer[] arr, Boolean flag, PrimeNumberFinder finder) {
+    public CustomThread(Integer[] arr, int startIndex, int endIndex, Boolean flag, PrimeNumberFinder finder) {
         this.arr = arr;
         this.flag = flag;
         this.finder = finder;
+        this.start = startIndex;
+        this.end = endIndex;
     }
 
     @Override
     public void run() {
-        for (Integer x : arr) {
-            if (finder.isNotPrime(x)) {
+        for (int i = start; i < end; i++) {
+            if (finder.isNotPrime(arr[i])) {
                 flag = Boolean.TRUE;
                 this.interrupt();
             }
