1ac86e04f9e7c9940344023a918b275263c447c0
Fagor <oleg-veber@mail.ru>
2023-03-06 20:07:45 +0700

    change thread logic (not creating new array)

--- Task_2_1_1/src/main/java/ru/nsu/fit/oop/veber/ThreadPrimeNumberFinder.java
+++ Task_2_1_1/src/main/java/ru/nsu/fit/oop/veber/ThreadPrimeNumberFinder.java
@@ -1,7 +1,5 @@
 package ru.nsu.fit.oop.veber;
 
-import java.util.Arrays;
-
 public class ThreadPrimeNumberFinder implements PrimeNumberFinder {
 
     private final Integer[] arr;
@@ -9,7 +7,7 @@ public class ThreadPrimeNumberFinder implements PrimeNumberFinder {
 
     public ThreadPrimeNumberFinder(Integer[] arr) {
         this.arr = arr;
-        this.threadCount = Thread.activeCount();
+        this.threadCount = Runtime.getRuntime().availableProcessors();
     }
 
     @Override
@@ -19,20 +17,13 @@ public class ThreadPrimeNumberFinder implements PrimeNumberFinder {
         CustomThread[] threads = new CustomThread[threadCount];
         int tempLength = arr.length / threadCount;
         int tempRest = arr.length % threadCount;
-        int tempPosition = 0;
+        int startIndex = 0;
+        int endIndex = tempLength + tempRest;
         for (int i = 0; i < threadCount; i++) {
-            Integer[] tempArray;
-            if (tempRest > 0) {
-                tempRest--;
-                tempArray = Arrays.copyOfRange(arr, tempPosition, tempPosition + tempLength + 1);
-                tempPosition += tempLength + 1;
-            } else {
-                tempArray = Arrays.copyOfRange(arr, tempPosition, tempPosition + tempLength);
-                tempPosition += tempLength;
-            }
-
-            threads[i] = new CustomThread(tempArray, false, this);
+            threads[i] = new CustomThread(arr, startIndex, endIndex, false, this);
             threads[i].start();
+            startIndex = endIndex;
+            endIndex += tempLength;
 
         }
         try {
