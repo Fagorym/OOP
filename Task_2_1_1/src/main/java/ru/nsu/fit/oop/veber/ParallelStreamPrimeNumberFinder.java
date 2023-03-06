5a959030ab29a5030fd19ce8480f363900cd4563
Fagor <oleg-veber@mail.ru>
2023-03-06 20:08:02 +0700

    change parallel stream logic

--- Task_2_1_1/src/main/java/ru/nsu/fit/oop/veber/ParallelStreamPrimeNumberFinder.java
+++ Task_2_1_1/src/main/java/ru/nsu/fit/oop/veber/ParallelStreamPrimeNumberFinder.java
@@ -1,7 +1,7 @@
 package ru.nsu.fit.oop.veber;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.Optional;
 
 public class ParallelStreamPrimeNumberFinder implements PrimeNumberFinder {
     private final Integer[] arr;
@@ -10,10 +10,11 @@ public class ParallelStreamPrimeNumberFinder implements PrimeNumberFinder {
         this.arr = arr;
     }
 
+
     @Override
     public Boolean haveNotPrime() {
         checkArrayCorrectness(arr);
-        List<Integer> list = Arrays.stream(arr).parallel().filter(this::isNotPrime).toList();
-        return !list.isEmpty();
+        Optional<Integer> list = Arrays.stream(arr).parallel().filter(this::isNotPrime).findAny();
+        return list.isPresent();
     }
 }
