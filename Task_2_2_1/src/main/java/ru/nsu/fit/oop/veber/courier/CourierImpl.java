02a1e3ebf71679072b79c416784a1f1536b83b22
Fagor <oleg-veber@mail.ru>
2023-03-27 18:50:07 +0700

    create courier bag

--- Task_2_2_1/src/main/java/ru/nsu/fit/oop/veber/courier/CourierImpl.java
+++ Task_2_2_1/src/main/java/ru/nsu/fit/oop/veber/courier/CourierImpl.java
@@ -3,19 +3,28 @@ package ru.nsu.fit.oop.veber.courier;
 import ru.nsu.fit.oop.veber.order.Pizza;
 import ru.nsu.fit.oop.veber.warehouse.Warehouse;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class CourierImpl implements Courier, Runnable {
     private final Warehouse warehouse;
+    private final Queue<Pizza> bag;
+
     private final int baggageCount;
 
     public CourierImpl(Warehouse warehouse, int baggageCount) {
         this.warehouse = warehouse;
         this.baggageCount = baggageCount;
+        this.bag = new ArrayDeque<>();
     }
 
     @Override
     public void deliverPizza() throws InterruptedException {
-        Pizza pizza = warehouse.getPizza();
-        System.out.println("Courier " + this + " received pizza " + pizza.toString());
+        while (!isBagFull()) {
+            Pizza pizza = warehouse.getPizza();
+            bag.add(pizza);
+            System.out.println("Courier " + this + " received pizza " + pizza.toString());
+        }
     }
 
     @Override
@@ -27,5 +36,9 @@ public class CourierImpl implements Courier, Runnable {
                 throw new RuntimeException(e);
             }
         }
+    }
+
+    private boolean isBagFull() {
+        return bag.size() == baggageCount;
     }
 }
