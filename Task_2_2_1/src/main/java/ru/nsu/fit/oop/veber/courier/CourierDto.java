7bb0d7a1ab3a5fcc137b2b2f0c17a53d93a0740c
Fagor <oleg-veber@mail.ru>
2023-03-28 10:06:48 +0700

    new Parser, new Gson, new Dto (records now working!)

--- Task_2_2_1/src/main/java/ru/nsu/fit/oop/veber/courier/CourierDto.java
+++ Task_2_2_1/src/main/java/ru/nsu/fit/oop/veber/courier/CourierDto.java
@@ -1,13 +1,4 @@
 package ru.nsu.fit.oop.veber.courier;
 
-public class CourierDto {
-    private final int baggageCount;
-
-    public CourierDto(int baggageCount) {
-        this.baggageCount = baggageCount;
-    }
-
-    public int getBaggageCount() {
-        return baggageCount;
-    }
+public record CourierDto(int baggageCount) {
 }
