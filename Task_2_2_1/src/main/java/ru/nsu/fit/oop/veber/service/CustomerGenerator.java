923e8039cd4be0c089a8c6fe64feeda2f5b4679e
Fagor <oleg-veber@mail.ru>
2023-03-31 13:00:54 +0700

    delegate 'generateCustomers' to service, delete repository

---
+++ Task_2_2_1/src/main/java/ru/nsu/fit/oop/veber/service/CustomerGenerator.java
@@ -0,0 +1,7 @@
+package ru.nsu.fit.oop.veber.service;
+
+import java.util.List;
+
+public interface CustomerGenerator {
+    List<Runnable> generate();
+}
--- Task_2_2_1/src/main/java/ru/nsu/fit/oop/veber/service/CustomerService.java
+++ Task_2_2_1/src/main/java/ru/nsu/fit/oop/veber/service/CustomerService.java
@@ -1,18 +1,19 @@
 package ru.nsu.fit.oop.veber.service;
 
-import ru.nsu.fit.oop.veber.customer.CustomerRepository;
+import ru.nsu.fit.oop.veber.customer.CustomerImpl;
 import ru.nsu.fit.oop.veber.pizzeria.Pizzeria;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class CustomerService implements Service {
+public class CustomerService implements Service, CustomerGenerator {
 
     private final Pizzeria pizzeria;
-
+    private final int MIN_CUSTOMER_COUNT = 3;
     private ExecutorService executorService;
-
     private boolean isAlive = true;
 
     public CustomerService(Pizzeria pizzeria) {
@@ -27,11 +28,22 @@ public class CustomerService implements Service {
 
     @Override
     public void run() {
-        final int MIN_CUSTOMER_COUNT = 3;
         executorService = Executors.newFixedThreadPool(MIN_CUSTOMER_COUNT);
-        List<Runnable> customerList = new CustomerRepository(MIN_CUSTOMER_COUNT, pizzeria).generateCustomers();
+        List<Runnable> customerList = generate();
         while (isAlive) {
             customerList.forEach(executorService::execute);
         }
+    }
+
+    @Override
+    public List<Runnable> generate() {
+        List<Runnable> customers = new ArrayList<>();
+        int customerCount = new Random().nextInt(3) + MIN_CUSTOMER_COUNT;
+        for (int i = 0; i < customerCount; i++) {
+            int pizzaCountInOrder = new Random().nextInt(3) + 1;
+            Runnable customer = new CustomerImpl(pizzeria, pizzaCountInOrder);
+            customers.add(customer);
+        }
+        return customers;
     }
 }
