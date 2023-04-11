8d148dc8662589666e537ae09ad467cf3fb943a0
Fagor <oleg-veber@mail.ru>
2023-04-11 10:34:10 +0700

    create console view snake view

--- Task_2_3_1/src/main/java/ru/nsu/fit/oop/veber/model/Snake.java
+++ Task_2_3_1/src/main/java/ru/nsu/fit/oop/veber/model/Snake.java
@@ -1,13 +1,31 @@
 package ru.nsu.fit.oop.veber.model;
 
+import ru.nsu.fit.oop.veber.utils.Direction;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Snake {
 
+    List<Snake> tail;
     private int x;
     private int y;
+
+    private Direction direction;
 
     public Snake(int x, int y) {
         this.x = x;
         this.y = y;
+        this.tail = new ArrayList<>();
+        this.direction = Direction.right;
+    }
+
+    public Snake(int x, int y, Direction direction) {
+        this.x = x;
+        this.y = y;
+        this.tail = new ArrayList<>();
+        this.direction = direction;
     }
 
     public int getX() {
@@ -24,5 +42,42 @@ public class Snake {
 
     public void setY(int y) {
         this.y = y;
+    }
+
+    public void checkFoodCollision(Food food) {
+        if (food.getX() == x && food.getY() == y) {
+            food.setX(new Random().nextInt(15));
+            food.setY(new Random().nextInt(15));
+            Snake newTail = switch (direction) {
+                case down -> new Snake(x, y - 1, direction);
+                case right -> new Snake(x - 1, y, direction);
+                case left -> new Snake(x + 1, y, direction);
+                case up -> new Snake(x, y + 1, direction);
+            };
+            tail.add(newTail);
+        }
+    }
+
+
+    public void checkBoxCollision() {
+        if (x == 0) {
+            if (y == 0 || y == 40) {
+                System.out.println("GAME OVER");
+            }
+        } else if (y == 0) {
+            if (x == 0 || x == 40) {
+                System.out.println("GAME OVER");
+            }
+        }
+    }
+
+
+    public void update() {
+        switch (direction) {
+            case right -> x += 1;
+            case left -> x -= 1;
+            case down -> y += 1;
+            case up -> y -= 1;
+        }
     }
 }
--- Task_2_3_1/src/main/java/ru/nsu/fit/oop/veber/view/ConsoleView.java
+++ Task_2_3_1/src/main/java/ru/nsu/fit/oop/veber/view/ConsoleView.java
@@ -5,6 +5,7 @@ import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import ru.nsu.fit.oop.veber.model.Food;
 import ru.nsu.fit.oop.veber.model.Snake;
 
 import java.io.IOException;
@@ -12,31 +13,38 @@ import java.io.IOException;
 public class ConsoleView implements View {
 
     private final int SCENE_LENGTH = 60;
-    private final int SCENE_HEIGHT = 15;
-
+    private final int SCENE_HEIGHT = 20;
+    private final Snake snake = new Snake(10, 4);
+    private final Food food = new Food(10, 15);
+    private Screen screen;
     private TextGraphics graphics;
+
+    private Terminal terminal;
 
     public ConsoleView() {
         createScene();
-        renderField();
-        renderSnake(new Snake(10, 4));
+        gameProcess();
+    }
+
+    private void renderFood(Food food) {
+        graphics.drawLine(food.getX(), food.getY(), food.getX(), food.getY(), '@');
     }
 
     private void renderField() {
+        graphics.drawLine(0, 0, SCENE_LENGTH, 0, '#');
+        for (int i = 1; i < SCENE_HEIGHT; i++) {
+            graphics.drawLine(0, i, 1, i, '#');
+            graphics.drawLine(SCENE_LENGTH - 1, i, SCENE_LENGTH, i, '#');
+        }
+        graphics.drawLine(0, SCENE_HEIGHT, SCENE_LENGTH, SCENE_HEIGHT, '#');
     }
 
     private void createScene() {
         try {
-            Terminal terminal = new DefaultTerminalFactory().createTerminal();
-            Screen screen = new TerminalScreen(terminal);
+            terminal = new DefaultTerminalFactory().createTerminal();
+            screen = new TerminalScreen(terminal);
             graphics = terminal.newTextGraphics();
-            graphics.drawLine(0, 0, SCENE_LENGTH, 0, '#');
-            for (int i = 1; i < SCENE_HEIGHT; i++) {
-                graphics.drawLine(0, i, 1, i, '#');
-                graphics.drawLine(SCENE_LENGTH - 1, i, SCENE_LENGTH, i, '#');
-            }
-            graphics.drawLine(0, SCENE_HEIGHT, SCENE_LENGTH, SCENE_HEIGHT, '#');
-            terminal.flush();
+            terminal.setCursorVisible(false);
         } catch (IOException exception) {
             System.out.println("Exception while creating terminal");
         }
@@ -46,5 +54,26 @@ public class ConsoleView implements View {
     @Override
     public void renderSnake(Snake snake) {
         graphics.drawLine(snake.getX(), snake.getY(), snake.getX(), snake.getY(), '$');
+    }
+
+
+    public void gameProcess() {
+        try {
+            while (true) {
+                screen.clear();
+                terminal.clearScreen();
+                renderField();
+                snake.update();
+                renderSnake(snake);
+                renderFood(food);
+                terminal.flush();
+                screen.refresh();
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException ex) {
+            System.out.println("THREAD WAS INTERRUPTED");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
