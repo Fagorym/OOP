4481892a17f046e360c21ca4088b8e6da362faa2
Fagor <oleg-veber@mail.ru>
2023-04-14 13:14:38 +0700

    delete redundant classes

--- Task_2_3_1/src/main/java/ru/nsu/fit/oop/veber/Main.java
+++ Task_2_3_1/src/main/java/ru/nsu/fit/oop/veber/Main.java
@@ -3,11 +3,19 @@ package ru.nsu.fit.oop.veber;
 import ru.nsu.fit.oop.veber.application.SnakeGameConsole;
 import ru.nsu.fit.oop.veber.application.SnakeGameGraphical;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        switch ("console") {
-            case "graphical" -> new SnakeGameGraphical().startGameProcess();
-            case "console" -> new SnakeGameConsole().startGameProcess();
+        System.out.println("Hello, it is a Snake Game!");
+        System.out.println("Press 1 to launch graphical version!");
+        System.out.println("Press 2 to launch console version!");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1 -> new SnakeGameGraphical().startGameProcess();
+            case 2 -> new SnakeGameConsole().startGameProcess();
+            default -> throw new IllegalArgumentException("Неверный ввод");
         }
     }
 }
