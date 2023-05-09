e9094610baa9c27e54b0659c539bcb93d197ccd7
Fagor <oleg-veber@mail.ru>
2023-05-09 17:42:52 +0700

    change game box

--- Task_2_3_1/src/main/java/ru/nsu/fit/oop/veber/model/CollisionChecker.java
+++ Task_2_3_1/src/main/java/ru/nsu/fit/oop/veber/model/CollisionChecker.java
@@ -7,7 +7,7 @@ public class CollisionChecker {
     private final ObjectType[][] objects;
 
     public CollisionChecker(int height, int length) {
-        objects = new ObjectType[height][length];
+        objects = new ObjectType[length][height];
         Arrays.stream(objects).forEach(x -> Arrays.fill(x, ObjectType.NOTHING));
     }
 
